8f74f3d7ab539a2b8fcfd98f17ec0b0fb04cc56f
Magnomos <tu.wang94@gmail.com>
2016-12-18 19:38:59 +0100

    release

--- release/eklabs.angularStarterPack.js
+++ release/eklabs.angularStarterPack.js
@@ -284,11 +284,67 @@ angular.module('eklabs.angularStarterPack.forms')
             }
         }
     });
+/**
+ * Created by Wang on 03/11/2016.
+ */
+'use strict';
+
+angular.module('eklabs.angularStarterPack.tchat',[
+
+]);
+/**
+ * Created by Wang on 03/11/2016.
+ */
+
+'use strict';
+
+angular.module('eklabs.angularStarterPack.tchat')
+    .directive('myTchat',function($log){
+        return {
+            templateUrl : 'eklabs.angularStarterPack/modules/tchat/directives/my-tchat/myTchatView.html',
+            scope : {
+                urlSocket        : '=',
+                callback    : '=?'
+            },link : function(scope){
+
+                /**
+                 *
+                 */
+                scope.$watch('urlSocket', function(myUrlSocket){
+                    scope.myUrlSocket = myUrlSocket;
+                });
+
+
+                /**
+                 * Default Actions
+                 * @type {{onValid: default_actions.onValid}}
+                 */
+                var default_actions = {
+                    onValid : function(urlSocket){
+                        $log.info('my socketApi is : ',urlSocket)
+                    }
+                };
+
+                /**
+                 * Catch Callback
+                 */
+                scope.$watch('callback', function(callback){
+                    if(callback instanceof Object){
+                        scope.actions = angular.extend({},default_actions,callback);
+                    }else{
+                        scope.actions = default_actions;
+                    }
+                });
+
+            }
+        }
+    });
 'use strict';
 
 angular.module('eklabs.angularStarterPack',[
     'eklabs.angularStarterPack.jsonEditor',
-    'eklabs.angularStarterPack.forms'
+    'eklabs.angularStarterPack.forms',
+    'eklabs.angularStarterPack.tchat'
 ]);
 'use strict';
 
